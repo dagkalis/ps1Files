1e285bdbf8df3853a72bdc1931603e068ea787be
g.dagkalis@orosimosoftware.gr <YLKYGy47qGS!$Ue4D7To>
2021-12-13 17:05:08 +0200

    new web logic also in gitpush

--- gitpush.ps1
+++ gitpush.ps1
@@ -28,8 +28,13 @@ if ($branch -eq "master") {
                 if ($confirmation -eq 'n') {exit}
                 $confirmation = Read-Host "merge? [y/n]"
         }
-        $folder = ((Get-Location) | Get-Item).Name
-        start "https://gitlab.com/orosimo/$folder"
+        $remote_url = git config --get remote.origin.url
+        if($remote_url -Match "gitlab.com"){
+            $end = $remote_url.split(":")[-1] -replace ".git", ""
+            start "https://gitlab.com/$end"
+        }else{
+            start $remote_url
+        }
 } else {
         echo "no commit comment"
 }
